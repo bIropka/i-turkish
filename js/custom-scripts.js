b76336c399401f2be48f3162b88056cdd3854ec4
Iegor Stetsiura <biropka@gmail.com>
2016-05-25 01:46:11 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -8,20 +8,4 @@ $(document).ready(function () {
     });
     /*** the end of the scroll script ***/
 
-    /*** window-callback ***/
-    $('.window-ok').click(function (event) {
-        $target = $(event.target);
-        if (!$target.closest($('.form-ok')).length) $('.window-ok').fadeOut();
-        if ($target.hasClass('form-ok-button')) $('.window-ok').fadeOut();
-    });
-    /*** the end ***/
-
-    /**** animation scripts *****/
-    window.sr = ScrollReveal();
-    sr.reveal('.block-content', {
-        reset: true,
-        mobile: false
-    });
-    /** end of animation scripts **/
-
 });
